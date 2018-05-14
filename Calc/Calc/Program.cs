92a982a26e7146136fdbaa5fb9f013cc4796f5f8
DarkHellbat <x23.targetx@gmail.com>
2018-05-14 15:35:38 +0300

    1 com

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length !=2)
+            {
+                Console.WriteLine("Введите 2 числа");
+                return;
+            }
+Console.WriteLine(Convert.ToInt64(args[0]) + Convert.ToInt64(args[0]));
+            Console.ReadKey();
+        }
+    }
+}
